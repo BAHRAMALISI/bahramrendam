b7e9e4db90463c7a7932808164fc3e8def337f91
BAHRAMALISI <106033048+BAHRAMALISI@users.noreply.github.com>
2022-06-11 18:52:07 +0200

    Create price.js

---
+++ price.js
@@ -0,0 +1,7 @@
+
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Sorry an error occured or you do not have money. Please make sure you have enough money.')
+});
+
